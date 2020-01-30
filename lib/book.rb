6f029bcd56110992a85a1f99dc06c0e39064e1bc
Roberto Livi <robertomlivi@outlook.com>
2020-01-30 14:21:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,9 @@
 class Book
   
-  attr_accessor :title
+  attr_accessor :title, :author
   
   def initialize(title)
     @title = title
-  end
-  
-  def author=(author)
-    @author = author
   end
   
   def author
