f4feca096c878a9f61b5619736559ba56e1da5dc
Roberto Livi <robertomlivi@outlook.com>
2020-01-30 14:26:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 class Shoe
   
-  attr_accessor :brand
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand
+  end
+  
+  def cobble(condition)
+    "The shoe has been repaired"
+    @condition = "New"
   end
 end
 
