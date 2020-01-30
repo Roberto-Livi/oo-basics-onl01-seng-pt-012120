126810068b4099f349f8f8b3b314660e75f0dedd
Roberto Livi <robertomlivi@outlook.com>
2020-01-30 14:10:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,13 @@
 class Shoe
+  
+  attr_accessor :brand, :color, :size, :matieral, :condition
+  
+  def initialize(brand, color, size, material, condition)
+    @brand = brand
+    @color = color
+    @material = material
+    @condition = condition
+  end
+  
+  
 end
